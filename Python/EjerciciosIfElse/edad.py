1ae844059911f13174cb5dfb0ec2e3e2f5851bdc
Brayan <brayantandap@gmail.com>
2023-04-28 15:08:43 -0500

    Verificacion de tipo de datos

--- Python/EjerciciosIfElse/edad.py
+++ Python/EjerciciosIfElse/edad.py
@@ -1,10 +1,14 @@
 """System module"""
-edad = int(input("Introduce tu edad: "))
-if edad >= 60:
-    print("Tiene descuento en su ticket.")
-elif edad >= 18:
-    print("Es mayor de edad.")  # agregar if, typeoff
-else:
-    print("Es menor de edad, no puede entrar.")
+edad = input("Introduce tu edad: ")
+if edad.isdigit():
+    edad = int(edad)
+    if edad >= 60:
+        print("Tiene descuento en su ticket.")
+    elif edad >= 18:
+        print("Es mayor de edad.")
+    else:
+        print("Es menor de edad, no puede entrar.")
 
-print("Edad comprobada.")
+    print("Edad comprobada.")
+else:
+    print("La edad comprobada no es un numero.")
